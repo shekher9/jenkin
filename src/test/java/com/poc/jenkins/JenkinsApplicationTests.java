b931e58a419c4338347fd85d289d967e874975cc
shekher <sundershekher9@gmail.com>
2022-03-14 16:14:17 +0530

    test class and yml file changes

--- src/test/java/com/poc/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/poc/jenkins/JenkinsApplicationTests.java
@@ -1,10 +1,16 @@
 package com.poc.jenkins;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class JenkinsApplicationTests {
 
@@ -13,6 +19,7 @@ class JenkinsApplicationTests {
 	@Test
 	public void contextLoads() {
 		log.info("Test case executed.....");
+		Assertions.assertTrue(true);
 	}
 
 }
